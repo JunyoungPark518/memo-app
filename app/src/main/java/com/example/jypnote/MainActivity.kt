872d5df6c00178ff8f8414b8fd705140cf5e26c9
jypark <jyp@notiplus.co.kr>
2019-10-02 18:30:03 +0900

    Retrofit, OkHttp 적용

--- app/src/main/java/com/example/jypnote/MainActivity.kt
+++ app/src/main/java/com/example/jypnote/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.jypnote.ui.main.adapter.SectionsPagerAdapter
+import com.example.jypnote.ui.main.api.ApiClient
 import com.example.jypnote.ui.main.api.ApiInterface
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var api: ApiInterface? = null
+    internal var apiInterface: ApiInterface? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +18,15 @@ class MainActivity : AppCompatActivity() {
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         view_pager.adapter = sectionsPagerAdapter
         tabs.setupWithViewPager(view_pager)
-//        val retrofit = ApiClient.getClient().create(ApiInterface.class)
+
+        // https://jongmin92.github.io/2018/01/29/Programming/android-retrofit2-okhttp3/
+        apiInterface = ApiClient.getClient().create(ApiInterface::class.java)
+        val call = apiInterface!!.selectBankList()
 
         fab.setOnClickListener {view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
     }
 }
